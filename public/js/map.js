46f525e3379fcc86b61e980372aebc25d27669fd
Pablo Lopez <pablo_mtv@hotmail.com>
2020-09-04 17:48:00 -0300

    Agregue detalles a la Pantalla principal y mapa

--- public/js/map.js
+++ public/js/map.js
@@ -3,3 +3,7 @@ var map = L.map('main_map').setView([-34.6012424,-58.3861497], 13);
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+L.marker([-34.6012424,-58.3861497]).addTo(map);
+L.marker([-34.596932,-58.3808287]).addTo(map);
+L.marker([-34.599564,-58.3778777]).addTo(map);
